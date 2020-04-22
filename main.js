fab96edcf34063bad8412277ac4f78ae578cf089
Chronowork1 <jianxie6464@gmail.com>
2020-04-22 02:35:20 -0700

    Completed list-view, pulling information from airtable

--- main.js
+++ main.js
@@ -12,35 +12,36 @@ function getParameterByName(name, url) {
 //
   var getAllRecords = function() {
     $.getJSON(
-      "https://api.airtable.com/v0/appJb4yWqdpQKLPMl/Universe?api_key=keyhIKEvxWbZNk1bF",
+      "https://api.airtable.com/v0/appJb4yWqdpQKLPMl/Universe?api_key=keyhIKEvxWbZNk1bF&view=Grid view",
       function(airtable) {
         var html = [];
         $.each(airtable.records, function(index, record) {
           var id = record.id;
+          console.log(id)
           var Features = record.fields["Features"];
           var Images = record.fields["Images"];
           var Description = record.fields["Description"];
-          html.push(listView(id, Features, Images, Description));
+          html.push(listView(id, Features, Description, Images));
         });
         $(".list-view").append(html);
       }
     );
   };
 
-  var listView = function(id, Images, Features, Description) {
+  var listView = function(id, Features,Description, Images) {
     return `
-      <div class="card border-dark" style="width: 18rem;">
-        ${Images ? `<img src="${Images[0].url}">` : ``}
+      <div>
+        ${Images ? `<img src="${Images[0].url}" class="spaceImg">` : ``}
         <div class="card-body">
-            <h2>${Features}</h2>
-            <p>${Description}</p>
+          <h2>${Features}</h2>
+          <p>${Description}</p>
         </div>
       </div>
     `;
   };
 
 var id = getParameterByName("id");
-if (id) {
+if(id) {
   getOneRecord(id);
 } else {
   getAllRecords();
