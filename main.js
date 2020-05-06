a726c0e8b52e82a74c7ae0b87ec52242a6c3e558
Chronowork1 <jianxie6464@gmail.com>
2020-05-06 02:58:15 -0700

    Detail View of Project Completed

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ function getParameterByName(name, url) {
         var html = [];
         $.each(airtable.records, function(index, record) {
           var id = record.id;
-          console.log(id)
           var Features = record.fields["Features"];
           var Images = record.fields["Images"];
           var Description = record.fields["Description"];
@@ -35,12 +34,35 @@ function getParameterByName(name, url) {
           ${Images ? `<img src="${Images[0].url}" class='spaceImg'>` : ``}
         </div>
         <div class="card-body info">
-          <h2>${Features}</h2>
+          <h2><a href="index.html?id=${id}">${Features}</a></h2>
           <p>${Description}</p>
         </div>
       </div>
     `;
   };
+
+  var getOneRecord = function(id){
+    $.getJSON(
+      `https://api.airtable.com/v0/appJb4yWqdpQKLPMl/Universe/${id}?api_key=keyhIKEvxWbZNk1bF`,
+      function(record) {
+        var html = [];
+        var Description = record.fields["Description"];
+        html.push(detailView(Description));
+        $(".detail-view").append(html);
+      }
+    )
+  }
+
+  var detailView = function(Description){
+    return `
+    <div>
+      <div class="card-body info">
+        <p>${Description}</p>
+      </div>
+    </div>
+    `
+  };
+  console.log(detailView);
 
 var id = getParameterByName("id");
 if(id) {
