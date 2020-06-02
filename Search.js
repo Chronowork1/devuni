d8c4170c674e146e88d3d5933f46e89fb7e6742b
Chronowork1 <jianxie6464@gmail.com>
2020-06-01 19:19:49 -0700

    Updated cors-everywhere, Search.css, and modify Search.js

--- Search.js
+++ Search.js
@@ -1,17 +1,22 @@
 $(document).ready(function(){
 
+    //#SearchButton is clicked
+    //Get the value from SearchInput
+    //Call the callNasaAPI() function with the input value
     $("#SearchButton").click(function(){
         var searchInput = $("#SearchInput").val();
         callNasaAPI(searchInput);
     });
 
+    
     function callNasaAPI(search){
         var nasaURL = nasaSearchInput(search);
-        $.ajax({
+        $.ajaxPrefilter({
             url: nasaURL,
             method: "GET",
             success: function(response){
-                var url = response.collection.items[0].href
+                //console.log(response.collection.href)
+               var url = response.collection.href
                 console.log(url)
                 appendToRoot(url);
             }
@@ -24,9 +29,21 @@ $(document).ready(function(){
         $("#root").append("<img src="+srcURL+">");
     }
     
-    function nasaSearchInput(search){
-        var nasaURL = "https://images-api.nasa.gov/search?q="+search;
-        return nasaURL;
-    }
+    //function nasaSearchInput(search){
+    //    var nasaURL = 'https://cors-anywhere.herokuapp.com/' + "https://images-api.nasa.gov/search?q="+search;
+    //    return nasaURL;
+    //}
+
+    jQuery.ajaxPrefilter(function nasaSearchInput(search){
+        if(search.crossDomain && jQuery.support.cors){
+            var nasaURL = "https://cors-anywhere.herokuapp.com/" + "https://images-api.nasa.gov/search?q="+search;
+            return nasaURL;
+        }
+    });
     
+    /*jQuery.ajaxPrefilter(function(options) {
+        if (options.crossDomain && jQuery.support.cors) {
+            options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+        }
+    });*/
 });
